fff11e2261197002461fbf3c1de3a95af5af26bc
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-07-19 21:42:14 +0200

    Initial commit

---
+++ precache-manifest.e68caeb2cc321969e43ee4c0bbe05329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9c0069e7eb666740e8c9e43dfeffa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a86fb24580346edeed16",
+    "url": "/static/css/main.f60a92d1.chunk.css"
+  },
+  {
+    "revision": "bcecc014701b3df52e68",
+    "url": "/static/js/2.ce485a92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce485a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86fb24580346edeed16",
+    "url": "/static/js/main.ac22a10d.chunk.js"
+  },
+  {
+    "revision": "a0c46e4d78508e9189e0",
+    "url": "/static/js/runtime-main.ca1726d3.js"
+  }
+]);
