f63faf1784cb67dcd05ad3aa95645ae337d9c506
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-08-04 11:48:51 +0200

    new look and feel

---
+++ precache-manifest.63dadd648c21bfa106c88eaea281b209.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4317d8a4476f674cc86bb9ef79a434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a20d07d9570e208ab4f5",
+    "url": "/static/css/main.282a70a3.chunk.css"
+  },
+  {
+    "revision": "bc3a0a27b6a055d6119c",
+    "url": "/static/js/2.03b7c886.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.03b7c886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a20d07d9570e208ab4f5",
+    "url": "/static/js/main.96517da3.chunk.js"
+  },
+  {
+    "revision": "a0c46e4d78508e9189e0",
+    "url": "/static/js/runtime-main.ca1726d3.js"
+  },
+  {
+    "revision": "569d6308758bd5bbc82e79e2004bf442",
+    "url": "/static/media/Twitter-logo.569d6308.svg"
+  },
+  {
+    "revision": "5871602b3cce68db0632be9353b43e15",
+    "url": "/static/media/github-mark.5871602b.svg"
+  },
+  {
+    "revision": "dd98389508c63550a9bd6f3043b261d6",
+    "url": "/static/media/linkedin.dd983895.svg"
+  },
+  {
+    "revision": "0582156d8bda3d20659788270a36c224",
+    "url": "/static/media/oliver-mini.0582156d.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0f5da255c6ef896083020413129ce2a.js"
+  "/precache-manifest.63dadd648c21bfa106c88eaea281b209.js"
 );
 
 self.addEventListener('message', (event) => {
