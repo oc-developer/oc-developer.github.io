243d26fa7f2016781646bcf28eddc2529d198cf1
Oliver Carlsson <oliverc@Olivers-Air.lan>
2020-08-26 21:03:53 +0200

    create test page

--- src/Projects/DynamicLetters/DynamicLetters.tsx
+++ src/Projects/DynamicLetters/DynamicLetters.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './DynamicLetters.scss'
 import FloatyLetters from '../../FloatyLetters/FloatyLetters'
 import DemoLetter from './DemoLetter/DemoLetter'
+import TestPage from './TestPage/TestPage'
 
 export default function DynamicLetters() {
     const placeholder = 'HELLO!'
